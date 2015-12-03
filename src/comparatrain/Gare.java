585c1c21ec99b1f08927c2e5a79785a001b0f387
HERNANDEZ Vincent <h12019633@tempo21-2.luminy.univ-amu.fr>
2015-12-03 16:59:02 +0100

    gare siege

--- src/comparatrain/Gare.java
+++ src/comparatrain/Gare.java
@@ -7,7 +7,15 @@ package comparatrain;
  * @author Vivien Galuchot - Vincent Hernandez
  *
  */
-public class Gare {
-	int id;
-	String ville;
+public class Gare implements Evaluable{
+	protected int id;
+	protected String ville;
+	
+	public Gare(String v){
+		ville=v;
+	}
+	
+	public int eval(){
+		return 0;
+	}
 }
--- src/comparatrain/Siege.java
+++ src/comparatrain/Siege.java
@@ -7,7 +7,14 @@ package comparatrain;
  * @author Vivien Galuchot - Vincent Hernandez
  *
  */
-public class Siege {
+public class Siege implements Evaluable{
 	int id;
 
+	public Siege(int i){
+		id=i;
+	}
+	
+	public int eval(){
+		return 0;
+	}
 }
